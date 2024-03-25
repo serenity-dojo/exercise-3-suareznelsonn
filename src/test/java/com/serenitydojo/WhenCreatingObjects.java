1033bbb6a3a074b2c4b70b2f7825f421e454e772
suareznelsonn <suareznelsonn@gmail.com>
2024-03-25 16:46:56 -0500

    Commit Exercise 3 - CreatingObjects suareznelsonn

--- src/test/java/com/serenitydojo/WhenCreatingObjects.java
+++ src/test/java/com/serenitydojo/WhenCreatingObjects.java
@@ -4,4 +4,16 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WhenCreatingObjects {
+    @Test
+    public void creating_a_dog() {
+        String name = "Fido";
+        String favoriteToy = "Bone";
+        int age = 5;
+
+        // Create an object of CAT class
+        Dog fido = new Dog(name,favoriteToy,age);
+        Assert.assertEquals(fido.getName(),name);
+        Assert.assertEquals(fido.getFavoriteToy(),favoriteToy);
+        Assert.assertEquals(fido.getAge(),age);
+    }
 }
