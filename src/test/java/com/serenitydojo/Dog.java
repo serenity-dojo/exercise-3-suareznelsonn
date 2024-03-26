c0550c34997c464ab6d928445c2f7f4c83028c05
suareznelsonn <suareznelsonn@gmail.com>
2024-03-26 16:39:24 -0500

    Commit Exercise 3A - Java Fields and methods suareznelsonn

--- src/test/java/com/serenitydojo/Dog.java
+++ src/test/java/com/serenitydojo/Dog.java
@@ -5,6 +5,8 @@ public class Dog {
     private String favoriteToy;
     private int age;
 
+    public static final String DOG_NOISE = "Woof";
+
     public Dog(String name, String favoriteToy, int age) {
         this.name = name;
         this.favoriteToy = favoriteToy;
@@ -21,5 +23,14 @@ public class Dog {
 
     public int getAge() {
         return age;
+    }
+
+    public void makeNoise() {
+        System.out.println(DOG_NOISE);
+    }
+
+    public boolean isFed() {
+        boolean b = true;
+        return b;
     }
 }
